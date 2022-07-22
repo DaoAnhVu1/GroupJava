600162b4858d7bec2961939e9f2cff049b4bbee5
Tien Bui <richiez.bui@gmail.com>
2022-07-22 14:21:02 +0700

    Added data

--- code/test.java
+++ code/test.java
@@ -1,22 +1,16 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 import Products.Product;
 import Users.Guess;
 
 public class test {
     public static void main(String[] args) {
-        Main.loadProductData("./data/Console.csv");
-        ArrayList<Product> allItemsList = new ArrayList<>();
-        for (String category : Product.productMap.keySet()) {
-            for (Product product : Product.productMap.get(category)) {
-               allItemsList.add(product);
-            }
+
+        for (int i = 0; i < 20; i++) {
+            String id = UUID.randomUUID().toString();
+            System.out.println(id);
         }
 
-        Guess.quicksortPrice(allItemsList, 0, allItemsList.size()-1);
-
-        for (Product product : allItemsList) {
-            System.out.println(product.getProductPrice());
-        }
     }
 }
